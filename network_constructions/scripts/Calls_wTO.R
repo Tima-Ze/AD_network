b172e578327a25fb914da87d37fd85e99b1d930f
Fatemeh Zebardast <76562916+Tima-Ze@users.noreply.github.com>
2025-01-08 21:14:29 +0100

    Simplify input file handling in Calls_wTO.R by removing redundant input directory reference

--- network_constructions/scripts/Calls_wTO.R
+++ network_constructions/scripts/Calls_wTO.R
@@ -1,9 +1,8 @@
 require(data.table)
 
-input_dir="../Data/"
 output_dir="../Results/Raw_wTO/"
 
-#Pass arguments including N (number of bootstrap), file (input file/gene count table), and save (resulted wTO network) to the script.
+#Pass arguments including N (number of bootstrap), file (input file), and save (wTO network) to the script.
 args=(commandArgs(TRUE))
 N <- args[1]
 file <- args[2]
@@ -16,7 +15,7 @@ print(file)
 ##This calls two functions that are modified wTO source Rscripts, which are modified to accelerate the process
 source("./wTO_Functions_snakemake.R")
 
-input = read.delim(paste0(input_dir, file)) %>%
+input = read.delim(file) %>%
   as.data.frame()
 
 wto = wTOFast_edit(Data = input, n = as.numeric(N))
