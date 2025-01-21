8d9268bdc420518e1b1fe417616111996574e0e6
Fatemeh Zebardast <76562916+Tima-Ze@users.noreply.github.com>
2025-01-21 17:30:45 +0100

    Refactor argument handling and improve code formatting in cns_function.R

--- network_constructions/bin/cns_function.R
+++ network_constructions/bin/cns_function.R
@@ -4,11 +4,11 @@ library(dplyr)
 library(data.table)
 library(igraph)
 
-# Pass arguments 
+# Pass arguments
 args <- (commandArgs(TRUE))
 input_dir <- args[1]
 output_dir <- args[2]
-wkdir <- args[2]
+wkdir <- args[3]
 
 # set the working directory
 setwd(wkdir)
@@ -19,24 +19,23 @@ if (!dir.exists(file.path(output_dir))) {
   dir.create(file.path(output_dir), recursive = TRUE)
 }
 
-for (i in dir(input_dir, pattern = '.txt', full.names = T)) {
+for (i in dir(input_dir, pattern = ".txt", full.names = T)) {
   fread(i, check.names = F, header = T) %>%
-    select(Node.1,  Node.2,  wTO) %>%
-    graph_from_data_frame(., directed = F) %>% 
-    assign(gsub('.txt', '', basename(i)), ., envir = .GlobalEnv)
+    select(Node.1, Node.2, wTO) %>%
+    graph_from_data_frame(., directed = F) %>%
+    assign(gsub(".txt", "", basename(i)), ., envir = .GlobalEnv)
   rm(i)
 }
 
 source("bin/cns_weight_function.R")
 
-#treat_Consensus
+# treat_Consensus
 intersection(treat1, treat2) %>%
-  wTO_consensus() %>% 
-  fwrite(., paste0(output_dir,'cns_treat.txt'), sep="\t", quote = F)
+  wTO_consensus() %>%
+  fwrite(., paste0(output_dir, "cns_treat.txt"), sep = "\t", quote = F)
 
 
-#control_Consensus
-intersection(control1, control2) %>% 
-  wTO_consensus() %>% 
-  fwrite(., paste0(output_dir,'cns_control.txt'), sep="\t", quote = F)
-
+# control_Consensus
+intersection(control1, control2) %>%
+  wTO_consensus() %>%
+  fwrite(., paste0(output_dir, "cns_control.txt"), sep = "\t", quote = F)
